8c74e93dc0a24598bb7f694d1fd628236cb6dcd3
sebastian montoya <sebitasm404@gmail.com>
2025-12-19 18:30:58 -0500

    desplegando5

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,1 +1,7 @@
-export const API_URL = "http://localhost:3001/api";
+
+// Esto detecta si estás en el navegador local o en la web
+const API_URL = window.location.hostname === 'localhost' 
+    ? 'http://localhost:3001/api' 
+    : 'https://app-tareas-production.up.railway.app';
+
+export default API_URL;
